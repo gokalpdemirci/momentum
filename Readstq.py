dd5902574fb078ff92dd3abd4791875e4fb9c198
gokalpdemirci <gokalpdemirci@gmail.com>
2017-10-13 20:30:11 -0500

    Add files via upload

--- Readstq.py
+++ Readstq.py
@@ -9,6 +9,8 @@ class readSTQFormat:
 	
 	def __init__(self):
 			
+			totalList = []
+			dates = []
 			self.table = np.array([])
 			self.adj_table = []
 			self.per_adj_table = []
@@ -16,38 +18,60 @@ class readSTQFormat:
 			allFiles = [r'data\5min-03may2017-19may2017\us\nasdaq stocks\2\tsla.us.txt', 
 						#r'data\5min-22may2017-07june2017\us\nasdaq stocks\2\tsla.us.txt',
 						#r'data\5min-08june2017-23june2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-26june2017-12july2017\us\nasdaq stocks\2\tsla.us.txt'
+						#r'data\5min-26june2017-12july2017\us\nasdaq stocks\2\tsla.us.txt'
 						]
 			
 			for filename in allFiles:
-				self.readSingleFile(filename)
-
+				d, l = self.readSingleFile(filename)
+				dates.extend(d)
+				totalList.extend(l)
+			
+			
+			if len(totalList)%78 == 0:
+				self.table = np.array(totalList)
+				self.table.shape =  (int(len(totalList)/78), 78)
+			else:
+				print('Error: Total List length is not a multiple of 78.')
+			
+			
+				
+				
 	def readSingleFile(self, filename):
 			
-			values = np.loadtxt(filename, delimiter=',', usecols=[2,3,4,5,6], skiprows=1)
+			values = np.loadtxt(filename, delimiter=',', usecols=[2], skiprows=1)
 			dateTimes = np.loadtxt(filename, delimiter=',', usecols=[0,1], skiprows=1, dtype = np.str)
-			print(values)
-			print(dateTimes)
-			nRow, nCol = values.shape
-			print(nRow)
-			print(nCol)
-			if nRow%78 == 0:
-				print("yes")
-				reshape(values[:,0], (78, nRow/78))
-				print(values)
-			else:
-				print("no")
-				
-			day_index = 0
-			time_index = 0
-			
-			day = []
-			newd = 0
-			for i in range(nRow):
-				if dateTimes[0][1]=='22:00:00':
-						newd = 1
-				else
+			dates = np.unique(dateTimes[:,0])
+			valuesList = values.tolist()
 
-	
+
+			if len(values)%78 == 0:
+				print("File seems to be clean: " + filename)
+			else:
+				print("File has missing data: " + filename)
+				offset = 0 #offset between indexing valuesList and values as we go on updating one and keeping the other smaller 
+				prev = 0
+				for i in range(1, len(values)):
+					differ = datetime.datetime.strptime(dateTimes[i][1], '%H:%M:%S') - datetime.datetime.strptime(dateTimes[prev][1], '%H:%M:%S')
+					
+					if  differ.seconds!=300 and differ.seconds!=63300:
+						if dateTimes[i][0] == dateTimes[prev][0]:
+							numberofmissing = int((differ.seconds/300) - 1)
+							end1 = values[prev]
+							end2 = values[i] 
+							for j in range(numberofmissing):
+								valuesList.insert(offset+j+prev+1, end1 + (end2-end1)/(numberofmissing +1))
+							offset += numberofmissing 
+							print("Corrected " + str(numberofmissing) + " missing values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
+						#else:
+							#handle missing days
+					prev = i
+
+			return dates, valuesList
+
+
+
+
+
+
 
 red1 = readSTQFormat()
