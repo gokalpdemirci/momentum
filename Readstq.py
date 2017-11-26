45bfcf59c482dade3f236158e97954642a3d17c6
gokalpdemirci <gokalpdemirci@gmail.com>
2017-11-26 00:47:38 -0600

    Add files via upload

--- Readstq.py
+++ Readstq.py
@@ -14,28 +14,12 @@ from sklearn.svm import LinearSVR
 
 class readSTQFormat:
 
-	def __init__(self):
+	def __init__(self, allFiles):
 
 			totalList = []
 			dates = []
-			self.table = np.array([])
 			self.adj_table = []
 			self.per_adj_table = []
-
-			allFiles = [r'data\5min-03may2017-19may2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-22may2017-07june2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-08june2017-23june2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-26june2017-13july2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-13july2017-28july2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-31july2017-15august2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-17august2017-01september2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-05september2017-19september2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-21september2017-06october2017\us\nasdaq stocks\2\tsla.us.txt',
-						r'data\5min-09october2017-24october2017\us\nasdaq stocks\2\tsla.us.txt',
-
-						]
-
-
 
 
 			values = [pd.read_csv(filename, usecols=[0,1,2,5,6]) for filename in allFiles]
@@ -47,156 +31,34 @@ class readSTQFormat:
 				if s == 'Open' and i != '15:35:00':
 					del table[('Open', i)]
 
+			#print(table)
+
 			if table[('Open', '15:35:00')].isnull().values.any():
-				print('Opening values have non!')
+				print('Readstq Warning: Opening values have non!')
+
+			table.dropna(inplace = True)
+			#table.fillna(-99999, inplace=True)
 
 			for (s,i) in table:
 				if s == 'Close':
 					table[('Close', i)] = 10000*(table[('Close', i)] - table[('Open', '15:35:00')])/table[('Open', '15:35:00')]
-			table.dropna(inplace = True)
-			#table.fillna(-99999, inplace=True)
-			Xcols = [('Close', i) for (s,i) in table if '15:35:00'<=i<='16:30:00' and s == 'Close']
-			Xcols += [('Volume', i) for (s,i) in table if '15:35:00'<=i<='16:30:00' and s == 'Volume']
 
-			#print(Xcols)
-			#print(table[Xcols].head())
-
-			X = np.array(table[Xcols])
-			y = np.array(table[[('Close', i) for (s,i) in table if '16:50:00'<=i<='17:20:00' and s == 'Close']].mean(axis=1))
-
-			np.set_printoptions(linewidth=120)
-			#print(table.iloc[[1]].values)
-			#print(X[1])
-			#print(y[1])
-			print(X.shape)
-			#print(table.head())
-
-			X_train = X[:60]
-			X_test = X[60:]
-			y_train = y[:60]
-			y_test = y[60:]
-
-			scaler = StandardScaler()
-			scaler.fit(X_train)
-
-			model = LinearSVR()
-			model.fit(scaler.transform(X_train), y_train)
-
-			print(X_test[:,11])
-			print(model.predict(scaler.transform(X_test)))
-			print(y_test)
-			print(model.predict(scaler.transform(X_test)) - X_test[:,11])
-			print(table[[('Close', i) for (s,i) in table if '16:50:00'<=i<='17:20:00' and s == 'Close']].max(axis=1)[60:].values)
-			print(table[[('Close', i) for (s,i) in table if '16:50:00'<=i<='17:20:00' and s == 'Close']].min(axis=1)[60:].values)
-
-
-			print(model.score(scaler.transform(X_test), y_test))
-
-
-			"""" THIS WORKS GREAT UNTIL HERE
-					HANDLE NANs IN THE TABLE AND JOIN ALL TABLES
-					NOW EACH ROW IN THE TABLE IS 1 DAY AND EACH COLUMN IS A PARTICULAR TIME OF THE DAY"""
-
-
-	"""			dates.extend(d)
-				totalList.extend(l)
-
-
-			if len(totalList)%78 == 0:
-				self.table = np.array(totalList)
-				self.table.shape =  (int(len(totalList)/78), 78)
-				print('\n Files read succesfully! Number of days: ' + str(len(self.table)))
-			else:
-				print('Error: Total List length is not a multiple of 78.')
-	"""
-
-
-
-
-	"""
-	def readSingleFile(self, filename):
-
-			values = np.loadtxt(filename, delimiter=',', usecols=[2], skiprows=1)
-			dateTimes = np.loadtxt(filename, delimiter=',', usecols=[0,1], skiprows=1, dtype = np.str)
-			dates = np.unique(dateTimes[:,0])
-			valuesList = values.tolist()
-
-
-
-			values = pd.read_csv(filename, usecols=[0,1,2])
-			values = values.pivot(index='Date', columns='Time', values='Open')
-
-
-			print(values.head())
-
-
-
-
-
-				if len(values)%78 == 0:
-				print("File seems to be clean: " + filename)
-			else: # clean data
-				print("File has missing data: " + filename)
-				offset = 0 #offset between indexing valuesList and values as we go on updating one and keeping the other smaller
-				prev = 0
-				for i in range(1, len(values)):
-					differ = datetime.datetime.strptime(dateTimes[i][1], '%H:%M:%S') - datetime.datetime.strptime(dateTimes[prev][1], '%H:%M:%S')
-					if  differ.seconds!=300 and differ.seconds!=63300:
-						if dateTimes[i][0] == dateTimes[prev][0]:
-							numberofmissing = int((differ.seconds/300) - 1)
-							print("Missing values " + str(numberofmissing) + " values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-							end1 = values[prev]
-							end2 = values[i]
-							for j in range(numberofmissing):
-								valuesList.insert(offset+j+prev+1, end1 + (end2-end1)/(numberofmissing +1))
-							offset += numberofmissing
-							print("Corrected " + str(numberofmissing) + " missing values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-						elif dateTimes[i][1] == '15:35:00' and  dateTimes[prev][1] != '22:00:00':
-							differ = datetime.datetime.strptime('22:05:00', '%H:%M:%S') - datetime.datetime.strptime(dateTimes[prev][1], '%H:%M:%S')
-							numberofmissing = int((differ.seconds/300) - 1)
-							print("Missing values " + str(numberofmissing) + " values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-							for j in range(numberofmissing):
-								valuesList.insert(offset+j+prev+1, values[prev])
-							offset += numberofmissing
-							print("Corrected " + str(numberofmissing) + " missing values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-						elif dateTimes[i][1] != '15:35:00' and  dateTimes[prev][1] == '22:00:00':
-							differ = datetime.datetime.strptime(dateTimes[i][1], '%H:%M:%S') - datetime.datetime.strptime('15:35:00', '%H:%M:%S')
-							numberofmissing = int((differ.seconds/300) - 1)
-							print("Missing values " + str(numberofmissing) + " values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-							for j in range(numberofmissing):
-								valuesList.insert(offset+i-1-j, values[i])
-							offset += numberofmissing
-							print("Corrected " + str(numberofmissing) + " missing values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-						elif dateTimes[i][1] != '15:35:00' and  dateTimes[prev][1] != '22:00:00':
-							differ = datetime.datetime.strptime('22:05:00', '%H:%M:%S') - datetime.datetime.strptime(dateTimes[prev][1], '%H:%M:%S')
-							numberofmissing = int((differ.seconds/300) - 1)
-							print("Missing values " + str(numberofmissing) + " values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-							for j in range(numberofmissing):
-								valuesList.insert(offset+j+prev+1, values[prev])
-							offset += numberofmissing
-							print("Corrected " + str(numberofmissing) + " missing values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-
-							differ = datetime.datetime.strptime(dateTimes[i][1], '%H:%M:%S') - datetime.datetime.strptime('15:35:00', '%H:%M:%S')
-							numberofmissing = int((differ.seconds/300) - 1)
-							print("Missing values " + str(numberofmissing) + " values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-							for j in range(numberofmissing):
-								valuesList.insert(offset+i-1-j, values[i])
-							offset += numberofmissing
-							print("Corrected " + str(numberofmissing) + " missing values between " + dateTimes[prev][0] +' ' + dateTimes[prev][1] + " and " + dateTimes[i][0] + ' ' + dateTimes[i][1])
-
-						#else:
-							#handle missing days
-					prev = i
-
-			if len(valuesList)%78 != 0:
-				print('List length is not a multiple of 78: ' + str(len(valuesList)) + ' in ' + filename)
-
-			return dates, valuesList
-	"""
-
+			self.table = table
 
 
 
 
 if __name__ == '__main__':
-	red1 = readSTQFormat()
+	red1 = readSTQFormat(allFiles = [r'data\5min-03may2017-19may2017\us\nasdaq stocks\2\tsla.us.txt',
+						r'data\5min-22may2017-07june2017\us\nasdaq stocks\2\tsla.us.txt',
+						r'data\5min-08june2017-23june2017\us\nasdaq stocks\2\tsla.us.txt',
+						r'data\5min-26june2017-13july2017\us\nasdaq stocks\2\tsla.us.txt',
+						r'data\5min-13july2017-28july2017\us\nasdaq stocks\2\tsla.us.txt',
+						r'data\5min-31july2017-15august2017\us\nasdaq stocks\2\tsla.us.txt',
+						r'data\5min-17august2017-01september2017\us\nasdaq stocks\2\tsla.us.txt',
+						r'data\5min-05september2017-19september2017\us\nasdaq stocks\2\tsla.us.txt',
+						r'data\5min-21september2017-06october2017\us\nasdaq stocks\2\tsla.us.txt',
+						r'data\5min-09october2017-24october2017\us\nasdaq stocks\2\tsla.us.txt',
+					#starts with 14:35:00 instead of 15:35:00	r'data\5min-25october2017-09november2017\us\nasdaq stocks\2\tsla.us.txt',
+
+						])
--- strategy.py
+++ strategy.py
@@ -1,30 +1,170 @@
 # File: strategy.py
-import datetime
+import os.path
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import numpy as np
-from KMeans import ts_cluster
-from dtwDistance import DTWDistance
-from sklearn.cluster import DBSCAN
-from sklearn import metrics
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
-import scipy.cluster.hierarchy as hac
-from scipy.spatial import distance
-from sklearn import linear_model
-import statsmodels.api as sm
+from sklearn import linear_model, neighbors
+from sklearn.naive_bayes import GaussianNB
+import numpy.polynomial.polynomial as poly
 
 from ReadGoogle import readGoogleFormat
 from Readstq import readSTQFormat
 
+np.set_printoptions(linewidth=120)
+
+
+
+entrypoint = 10
+outWindowLength = 12
+expectedMoveInPricePercent = 80
+
+priceDegree = 2
+volumeDegree = 1
+
+allFiles = [r'data\5min-03may2017-19may2017\us\nasdaq stocks\2\tsla.us.txt',
+			r'data\5min-22may2017-07june2017\us\nasdaq stocks\2\tsla.us.txt',
+			r'data\5min-08june2017-23june2017\us\nasdaq stocks\2\tsla.us.txt',
+			r'data\5min-26june2017-13july2017\us\nasdaq stocks\2\tsla.us.txt',
+			r'data\5min-13july2017-28july2017\us\nasdaq stocks\2\tsla.us.txt',
+			r'data\5min-31july2017-15august2017\us\nasdaq stocks\2\tsla.us.txt',
+			r'data\5min-17august2017-01september2017\us\nasdaq stocks\2\tsla.us.txt',
+			r'data\5min-05september2017-19september2017\us\nasdaq stocks\2\tsla.us.txt',
+			r'data\5min-21september2017-06october2017\us\nasdaq stocks\2\tsla.us.txt',
+			r'data\5min-09october2017-24october2017\us\nasdaq stocks\2\tsla.us.txt',
+			#starts with 14:35:00 instead of 15:35:00	r'data\5min-25october2017-09november2017\us\nasdaq stocks\2\tsla.us.txt',
+			]
+
+
+if os.path.isfile('tempdata.csv'):
+	table = pd.read_csv('tempdata.csv', skipinitialspace=True, index_col=[0], header=[0, 1])
+else:
+	read = readSTQFormat(allFiles)
+	read.table.to_csv('tempdata.csv')
+	table = read.table
+
+print(table.head())
+
+XPricecols = [('Close', i) for (s,i) in table if '15:35:00'<=i<='20:00:00' and s == 'Close']
+XVolumecols = [('Volume', i) for (s,i) in table if '15:35:00'<=i<='20:00:00' and s == 'Volume']
+
+print("XPriceColumns: ", XPricecols)
+print(table[XPricecols].head())
+
+reduced_table = pd.DataFrame(index= table.index.values, columns = ['Price Coeff '+str(i) for i in range(priceDegree+1)]+
+										   ['Volume Coeff '+str(i) for i in range(volumeDegree+1)]+['classification'])
+
+
+
+for index, row in table.iterrows():
+	priceCoefs = poly.polyfit([i for i in range(entrypoint+1)], [0.0] + row[XPricecols[:entrypoint]].values.tolist(), priceDegree)
+	volumeCoefs = poly.polyfit([i for i in range(entrypoint+1)], [0.0] + [i/10000 for i in row[XVolumecols[:entrypoint]].values], volumeDegree)
+	classification = 0
+	if max(row[XPricecols[entrypoint:entrypoint+outWindowLength]].values.tolist()) - row[XPricecols[entrypoint-1]] >= expectedMoveInPricePercent:
+		classification = 1
+	elif row[XPricecols[entrypoint-1]] - min(row[XPricecols[entrypoint:entrypoint+outWindowLength]].values.tolist()) >= expectedMoveInPricePercent:
+		classification = -1
+	reduced_table.loc[index,:] = priceCoefs.tolist() + volumeCoefs.tolist() + [classification]
+	if '2017-05-0' in index:
+		print("Day: ", index, [0.0] + row[XPricecols[:entrypoint]].values.tolist(), 'enrty point: ', row[XPricecols[entrypoint-1]])
+		print("Coefs: ", priceCoefs.tolist() + volumeCoefs.tolist() + [classification])
+		plt.scatter([i+1 for i in range(entrypoint+outWindowLength)], row[XPricecols[:entrypoint+outWindowLength]].values.tolist())
+		plt.plot([i+1 for i in range(entrypoint)], [poly.polyval(i+1, priceCoefs) for i in range(entrypoint)],'-')
+		plt.show()
+
+
+
+print(reduced_table.head())
+
+X = np.array(reduced_table[['Price Coeff '+str(i) for i in range(priceDegree+1)] + ['Volume Coeff '+str(i) for i in range(volumeDegree+1)]])
+y = np.array(reduced_table['classification']).astype(int)
+
+numberOfDataPoints, dimensions = X.shape
+
+trainingSize = int(2* numberOfDataPoints/3)
+
+print("Number of Data Points: ", numberOfDataPoints, "Number of Training Examples: ", trainingSize)
+X_train = X[:trainingSize]
+X_test = X[trainingSize:]
+y_train = y[:trainingSize]
+y_test = y[trainingSize:]
+
+
+
+#scaler = StandardScaler()
+#scaler.fit(X_train)
+
+
+
+#  CLASSIFICATION OLMUYOR
+#  REGRESSION A GERI DON
+#
+
+
+
+model = GaussianNB() #neighbors.KNeighborsClassifier(n_neighbors=7, weights='distance')
+model.fit( #scaler.transform(X_train),
+			X_train,
+			y_train)
+
+#print(X_test)
+#print(model.predict(scaler.transform(X_test)))
+testResult = model.predict(X_test)
+print(testResult)
+print(y_test)
+Errcount = sum([1 for i, x in enumerate(testResult) if x != y_test[i] and x != 0])
+Rightcount = sum([1 for i, x in enumerate(testResult) if x == y_test[i] != 0 ] )
+
+print('Guessed right: ', Rightcount, ', guessed wrong: ', Errcount)
+
+mycolors =["red", "gold", "limegreen"]
+cmap =  colors.ListedColormap(mycolors)
+plt.scatter(X_train[:,0], X_train[:,1], c=[i+1 for i in y_train], cmap=cmap)
+plt.show()
+cmap =  colors.ListedColormap(mycolors)
+plt.scatter(X_test[:,0], X_test[:,1], c=[i+1 for i in y_test], cmap=cmap)
+plt.show()
+
+'''
+X = np.array(table[Xcols])
+y = np.array(table[[('Close', i) for (s,i) in table if '16:50:00'<=i<='17:20:00' and s == 'Close']].mean(axis=1))
+np.set_printoptions(linewidth=120)
+#print(table.iloc[[1]].values)
+
+#print(table.head())
+
+
+
+#scaler = StandardScaler()
+#scaler.fit(X_train)
+
+#model = LinearSVR()
+#model.fit(scaler.transform(X_train), y_train)
+
+print(X_test[:,11])
+#print(model.predict(scaler.transform(X_test)))
+print(y_test)
+#print(model.predict(scaler.transform(X_test)) - X_test[:,11])
+print(table[[('Close', i) for (s,i) in table if '16:50:00'<=i<='17:20:00' and s == 'Close']].max(axis=1)[60:].values)
+print(table[[('Close', i) for (s,i) in table if '16:50:00'<=i<='17:20:00' and s == 'Close']].min(axis=1)[60:].values.toList)
+
+
+	#print(model.score(scaler.transform(X_test), y_test))
 
 
 
 
 
 
-alldata = red1.per_adj_table + red2.per_adj_table + red3.per_adj_table 
+
+
+
+
+alldata = red1.per_adj_table + red2.per_adj_table + red3.per_adj_table #+ red4.per_adj_table
 print('Total number of days: '+str(len(alldata)))
 
-print(red1.table)
+#print(red1.table)
 
 
 Coeff = []
@@ -36,11 +176,11 @@ xaxis = np.array([i for i in range(len(alldata[0]))])
 #print('xaxis: '+str(len(xaxis)))
 
 train_beg = 0 #included
-train_end = 10  #not included
+train_end = 15  #not included
 test_beg = train_end #included
 test_end = len(alldata) #not included
 
-entrypoint = 13 #enter at (entrypoint)th data point. Use range(0:entrypoint) as day opening
+entrypoint = 18 #enter at (entrypoint)th data point. Use range(0:entrypoint) as day opening
 
 opening = [i[0:entrypoint] for i in alldata]
 dayendtargetbeg = 50 #included use as index
@@ -52,7 +192,7 @@ print('Entry Point: '+str(entrypoint) + ', Target Range: '+str(dayendtargetbeg) 
 
 
 for i in range(len(alldata)):
-	model = sm.OLS(opening[i], sm.add_constant([j for j in range(len(opening[i]))])).fit() 
+	model = sm.OLS(opening[i], sm.add_constant([j for j in range(len(opening[i]))])).fit()
 	Coeff.append(model.params[1])
 	Coeff0.append(model.params[0])
 	Avgs.append(sum(alldata[i][dayendtargetbeg:dayendtargetend])/(dayendtargetend-dayendtargetbeg))
@@ -81,6 +221,19 @@ plt.show()
 
 total_return = 0
 for i in range(test_beg,test_end):
+	if i%10 == 0:
+		model = sm.OLS(Avgs[i-16:i-1], sm.add_constant(Coeff[i-16:i-1])).fit()
+		#print(model.summary())
+		w0 = model.params[0]
+		w1 = model.params[1]
+		print('w0: '+str(w0)+', w1: '+str(w1))
+		plt.scatter(Coeff[train_beg:train_end], Avgs[train_beg:train_end])
+		for e, x, y in zip(Error[train_beg:train_end], Coeff[train_beg:train_end], Avgs[train_beg:train_end]):
+			plt.annotate(round(e, 0), (x, y))
+		plt.plot([-50, 30], [w0 + w1*(-50), w0 + w1*(30)], 'k-', lw=1)
+
+		plt.show()
+
 	p = alldata[i]
 	error = 0
 	for j in range(0,entrypoint):
@@ -122,9 +275,10 @@ for i in range(test_beg,test_end):
 				#plt.plot([0, entrypoint], [0 , Coeff[i]*entrypoint], 'k-', lw=1)
 				#plt.show()
 				total_return += p[entrypoint] - p[j]
-				break	
-			
+				break
+
 print('Total return: '+str(total_return) )
 
 plt.show()
 
+'''
